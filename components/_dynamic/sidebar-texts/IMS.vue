c4023c0dd908b4eb99b90c25fe1ba2d9a1a9e910
miduku <miduku11@gmail.com>
2020-06-24 16:28:21 +0200

    update tipping point texts

--- components/_dynamic/sidebar-texts/IMS.vue
+++ components/_dynamic/sidebar-texts/IMS.vue
@@ -5,6 +5,7 @@
 
   The warming climate however, changes the contrast between surface temperatures and causes monsoon alterations. As a warmer atmosphere can hold more moisture an increase in precipitation is presumed. Yet air pollution in India is also considered to be a possible factor for declining rainfall. Sulphur dioxide emissions, produced by industry and transport, discharge sulphates in the atmosphere that reflect solar radiation and cool down certain regions.
 
-  Thanks to this phenomenon the warming of surface temperature could have been concealed and increased rainfall repressed. But “the imprint of climate change is clearly manifest in the increasing frequency of extreme weather outcomes.” And “[i]n particular, monsoon rainfall extremes can cause substantial crop damage across the subcontinent.” Regarding how these negative impacts majorly affect India’s agriculture (the aspect of food security among other things) and economy, a prediction of changes is crucial. Thankfully according to IPCC “there is low confidence regarding changes in monsoons” with global warming of 1,5°C or 2 °C.
+  Thanks to this phenomenon the warming of surface temperature could have been concealed and increased rainfall repressed. But “the imprint of climate change is clearly manifest in the increasing frequency of extreme weather outcomes.” And “[i]n particular, monsoon rainfall extremes can cause substantial crop damage across the subcontinent.”
 
+  Regarding how these negative impacts majorly affect India’s agriculture (the aspect of food security among other things) and economy, a prediction of changes is crucial. Thankfully according to IPCC “there is low confidence regarding changes in monsoons” with global warming of 1,5°C or 2°C.
 </template>
