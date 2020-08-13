832cc5daf139278ba1c5a84e6e295184f4195994
Dustin Kummer <miduku11@gmail.com>
2020-08-13 20:44:32 +0200

    intro text is 2 columns now

--- components/TheIntroOverlay.vue
+++ components/TheIntroOverlay.vue
@@ -32,38 +32,45 @@
 
     <section id="intro-2">
       <div class="container">
-        <p>
-          But what is a tipping point? The climate has been warming up
-          persistently, entailing severe changes in CO2 levels in the
-          atmosphere, ocean heat content and global sea level rise. Due to their
-          <SourceAnchorLink to-id="1" />
-          impact, especially when combined, they hold the power to alter our
-          planet. These so-called “tipping points” are thresholds that are able
-          to push a system into an entirely new state, even with mere changes.
-          Exceeding this threshold could trigger a self-reinforcing process and
-          cause irreversible damage.
-        </p>
+        <div class="split">
+          <div>
+            <p>
+              But what is a tipping point? The climate has been warming up
+              persistently, entailing severe changes in CO2 levels in the
+              atmosphere, ocean heat content and global sea level rise. Due to
+              their
+              <SourceAnchorLink to-id="1" />
+              impact, especially when combined, they hold the power to alter our
+              planet. These so-called “tipping points” are thresholds that are
+              able to push a system into an entirely new state, even with mere
+              changes. Exceeding this threshold could trigger a self-reinforcing
+              process and cause irreversible damage.
+            </p>
 
-        <p>
-          Tipping points can be found in every part of the world, whether it is
-          the Greenland Ice Sheet far north or Coral Reefs in the Southern
-          Pacific. As they are all directly or indirectly linked to each other,
-          passing one may fuel others and lead to a cascade.
-        </p>
+            <p>
+              Tipping points can be found in every part of the world, whether it
+              is the Greenland Ice Sheet far north or Coral Reefs in the
+              Southern Pacific. As they are all directly or indirectly linked to
+              each other, passing one may fuel others and lead to a cascade.
+            </p>
+          </div>
 
-        <p>
-          They can be caused by natural fluctuations but since man-made
-          greenhouse gas levels keep on rising and the climate has already
-          heated by 1°C, tipping points may occur in unexpected ways, more
-          promptly and with graver consequences. There is no doubt about the
-          urgency of taking immediate international action and keeping global
-          temperatures as low as possible.
-        </p>
+          <div>
+            <p>
+              They can be caused by natural fluctuations but since man-made
+              greenhouse gas levels keep on rising and the climate has already
+              heated by 1°C, tipping points may occur in unexpected ways, more
+              promptly and with graver consequences. There is no doubt about the
+              urgency of taking immediate international action and keeping
+              global temperatures as low as possible.
+            </p>
 
-        <p>
-          Explore the following nine tipping points in detail and gain insight
-          into why they hold such great significance.
-        </p>
+            <p>
+              Explore the following nine tipping points in detail and gain
+              insight into why they hold such great significance.
+            </p>
+          </div>
+        </div>
 
         <div class="start-button">
           <Button class="button" @click="closeIntro">
@@ -157,6 +164,24 @@ export default {
           img {
             width: 2.5rem;
             animation: hovering 2s $easeInOutQuint infinite;
+          }
+        }
+      }
+    }
+
+    &#intro-2 {
+      .container {
+        max-width: unset;
+
+        .split {
+          display: flex;
+          flex-wrap: wrap;
+          justify-content: center;
+
+          > div {
+            /* max-width: 50%; */
+            max-width: calc(400px + 2rem);
+            padding: 0 1rem;
           }
         }
       }
