f44d25284561b035a54d691be35c21660fb51584
Dustin Kummer <miduku11@gmail.com>
2020-09-19 19:01:11 +0200

    update gifs

--- components/_dynamic/sidebar-texts/GIS.vue
+++ components/_dynamic/sidebar-texts/GIS.vue
@@ -7,7 +7,7 @@
   <span id="h-GIS-input-1" class="is-highlightable">Higher global temperatures mostly influence the surfaces of Greenland Ice Sheets.</span> <span id="h-GIS-output-1" class="is-highlightable">This initiates a self-reinforcing process where melting causes lower ice sheets.</span> Therefore shifting them to lower and warmer altitudes and resulting in further melting. <span id="h-GIS-output-0" class="is-highlightable">“Around half of the melt that the Greenland ice sheet experiences occurs at the surface.”</span>
   Linked to the surface ice decline is the darkening of it through various impacts. <span id="h-GIS-input-0" class="is-highlightable">This contributes to the albedo reduction and a snowline (the boundary between snow-covered and snow-free areas) migration to higher elevations as the ice sheet warms</span>. Thanks to the albedo effect, more sun energy can be reflected back by white snow.<SourceAnchorLink to-id="2" /> Meaning darker ice surfaces absorb more solar radiation and retain less meltwater. Ultimately this leads to yet more melting and water running off into the ocean.<SourceAnchorLink to-id="6" />
 
-  ![Greenland Ice Sheet Disintegration](~assets/gif/gis.gif)
+  ![Greenland Ice Sheet Disintegration](~assets/gif/GIS.gif)
 
   <span id="h-GIS-output-2" class="is-highlightable">Less meltwater can be retained because of it trickling down into the snowpack and filling the pores in the snow. When the water refreezes it radiates more heat, hindering the hold of any additional meltwater</span>. Other events leading to rising sea levels are: melting ice sheet bases and breaking off iceberg edges.<SourceAnchorLink to-id="2" />
 
--- components/_dynamic/sidebar-texts/WAIS.vue
+++ components/_dynamic/sidebar-texts/WAIS.vue
@@ -3,11 +3,11 @@
 
   Even though the West Antarctic Ice Sheet isn’t the biggest Antarctic region it could, if completely melted, raise global sea levels by around 3,3 meters. As a result global coastlines would be altered substantially.
 
-  ![West Antarctic Ice Sheet Loss](~assets/gif/wais.gif)
-
   <span id="h-WAIS-output-0" class="is-highlightable">Warmer temperatures are the major source for ice sheet loss.</span> Especially wind and ocean heat since great parts of the West Antarctic Ice Sheet lie beneath sea level.<SourceAnchorLink to-id="2" /> <span id="h-WAIS-input-1" class="is-highlightable">Grounded ice sheet bases thin and melt underwater on account of the warm ocean. Simultaneously warm winds increase ablation<SourceAnchorLink to-id="15" /> and surface melting, ultimately developing ice shelves that float toward the coast.</span>
 
   This so-called Marine Ice Sheet Instability is a self-sustaining event where the grounding line (meaning the boundary line underneath the glacier where the grounded ice sheet becomes a floating ice shelf) recedes. This lifts more ice off the seafloor and turns it into floating ice shelves. <span id="h-WAIS-input-2" class="is-highlightable">The ice then flows faster, from its interior towards the coast and southern ocean, which causes more thinning and lastly more lifted ice.<SourceAnchorLink to-id="16" /></span> <span id="h-WAIS-input-0" class="is-highlightable">Usually the lost ice gets replaced by fresh snowfall but increasing temperatures cause less snowfall.</span> <span id="h-WAIS-output-1" class="is-highlightable">The ice loss process accelerates as the ice sheet loses more ice to the ocean than it gains in snow, resulting in rising sea levels.<SourceAnchorLink to-id="2" /></span>
+
+  ![West Antarctic Ice Sheet Loss](~assets/gif/WAIS.gif)
 
   In Antarctica the Amundsen sea sector glaciers contribute the most to global sea levels.<SourceAnchorLink to-id="2" /> And although ice loss will take centuries to complete<SourceAnchorLink to-id="17" />, it is suspected that the West Antarctic Ice Sheet currently is in the early stages of marine ice sheet instability<SourceAnchorLink to-id="18" />. It’s proven that between 1992 and 2017 the ice loss rate has tripled.<SourceAnchorLink to-id="19" />
 </template>
